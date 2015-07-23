8f4cecb2954f6159a28ad6998c7872fef44313f3
RaulGM <raulgm83@gmail.com>
2015-07-23 17:59:04 +0200

    Edit questions

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -74,3 +74,26 @@ exports.create = function(req, res){
 	);
 };
 
+// GET /quizes/new
+exports.edit = function(req, res){
+	var quiz = req.quiz; //autoload de la instancia de quiz
+	res.render('quizes/edit', {quiz: quiz, errors: []});
+};
+
+// GET /quizes/create
+exports.update = function(req, res){
+	req.quiz.pregunta = req.body.quiz.pregunta;
+	req.quiz.respuesta = req.body.quiz.respuesta;
+	console.log("----> UPDATE");
+	req.quiz.validate().then(function(err){
+		console.log(err);
+		if(err){
+			res.render('quizes/edit', {quiz: req.quiz, errors: err.errors});
+		}else{
+			req.quiz.save({fields:["pregunta", "respuesta"]}).then(function(){
+				res.redirect('/quizes');
+			})
+		}
+		}
+	);
+};
